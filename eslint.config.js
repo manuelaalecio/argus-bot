07244ae1b820589498c51e3cf0a1b8f41475125a
Manuela Alecio <manuelaalecio.dev@gmail.com>
2025-02-02 16:27:16 -0300

    feat: config eslint and husky

--- eslint.config.js
+++ eslint.config.js
@@ -1,53 +1,22 @@
-import js from "@eslint/js";
+import eslint from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import pluginPrettier from 'eslint-plugin-prettier/recommended';
 
 export default [
-  js.configs.recommended,
+  eslint.configs.recommended,
+  prettier,
+  pluginPrettier,
   {
     languageOptions: {
-      ecmaVersion: "latest",
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {
+        process: 'readonly',
+        console: 'readonly',
+      },
     },
     rules: {
-      "arrow-spacing": ["warn", { before: true, after: true }],
-      "brace-style": ["error", "stroustrup", { allowSingleLine: true }],
-      "comma-dangle": ["error", "always-multiline"],
-      "comma-spacing": "error",
-      "comma-style": "error",
-      curly: ["error", "multi-line", "consistent"],
-      "dot-location": ["error", "property"],
-      "handle-callback-err": "off",
-      indent: ["error", "tab"],
-      "keyword-spacing": "error",
-      "max-nested-callbacks": ["error", { max: 4 }],
-      "max-statements-per-line": ["error", { max: 2 }],
-      "no-console": "off",
-      "no-empty-function": "error",
-      "no-floating-decimal": "error",
-      "no-inline-comments": "error",
-      "no-lonely-if": "error",
-      "no-multi-spaces": "error",
-      "no-multiple-empty-lines": ["error", { max: 2, maxEOF: 1, maxBOF: 0 }],
-      "no-shadow": ["error", { allow: ["err", "resolve", "reject"] }],
-      "no-trailing-spaces": ["error"],
-      "no-var": "error",
-      "no-undef": "off",
-      "object-curly-spacing": ["error", "always"],
-      "prefer-const": "error",
-      quotes: ["error", "single"],
-      semi: ["error", "always"],
-      "space-before-blocks": "error",
-      "space-before-function-paren": [
-        "error",
-        {
-          anonymous: "never",
-          named: "never",
-          asyncArrow: "always",
-        },
-      ],
-      "space-in-parens": "error",
-      "space-infix-ops": "error",
-      "space-unary-ops": "error",
-      "spaced-comment": "error",
-      yoda: "error",
+      'no-console': 'warn',
     },
   },
 ];
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { Client, Events, GatewayIntentBits } from "discord.js";
-import dotenv from "dotenv";
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+import dotenv from 'dotenv';
 dotenv.config();
 
 const token = process.env.DISCORD_TOKEN;
